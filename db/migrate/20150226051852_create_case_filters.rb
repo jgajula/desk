3b13c691d113f8604e5afe1a1751ae7d1d162ffa
Jagadeesh Gajula <jgajula@visa.com>
2015-02-26 20:50:57 -0800

    db migrations added [#ticket-num-goes-here]

---
+++ db/migrate/20150226051852_create_case_filters.rb
@@ -0,0 +1,8 @@
+class CreateCaseFilters < ActiveRecord::Migration
+  def change
+    create_table :case_filters do |t|
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   Fruits = Fruit.create([{ name: 'Apple' }, { name: 'banana' }])
+#   tree.create(name: 'Apple tree', fruit: fruits.first)
