6851c851bb6350d7d1635eadad62d82c73848e1d
Jagadeesh Gajula <jgajula@visa.com>
2015-02-26 20:49:44 -0800

    - Application configuration added - Desk.com API configuration added - Desk.com API wrapper added
    [#ticket-num-goes-here]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Salesforce::Application.config.secret_token = 'cf26d17ca6e0fbc4c357c81c3b5f3b2c3a394eb555c07fccb8d80fd5e95e5db21e50e02c788f83835a60fb6ece259e9f04b6973eae51d11c58f4a51998cac986'
